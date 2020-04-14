77e2314a891b0e7eafd67c922bb1bb9e54c5abae
Arvind Yalavarti <yalavartiarvind@gmail.com>
2020-04-14 15:02:56 -0400

    Add basic file structure

---
+++ main.py
@@ -0,0 +1,34 @@
+from argparse import ArgumentParser, ArgumentTypeError
+import os
+import tensorflow as tf
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sn
+import pandas as pd
+
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+gpu_available = tf.test.is_gpu_available()
+
+
+def parse_args():
+    pass
+
+
+def train():
+    pass
+
+
+def test():
+    pass
+
+
+def main():
+    pass
+
+
+# Make arguments global
+ARGS = parse_args()
+
+if __name__ == '__main__':
+    main()
