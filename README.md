9c4c55258be78e928bb75bfa2c7fba13a640b0ab
Arvind Yalavarti <yalavartiarvind@gmail.com>
2020-04-18 13:13:07 -0400

    Set up argparse in main.py

--- README.md
+++ README.md
@@ -12,3 +12,8 @@ an option `--gcp` flag to download the dataset from the GCP Storage bucket.
 
 Run `pytest` from the root directory to run all unit tests.
 
+To run the neural net model, use
+```
+python main.py [-h] {train, test} ...
+```
+Use the `-h` or `--help` flags to specify optional arguments. For example `python main.py train -h`.
--- main.py
+++ main.py
@@ -1,34 +1,102 @@
-from argparse import ArgumentParser, ArgumentTypeError
 import os
+from argparse import ArgumentParser, ArgumentTypeError
+
 import tensorflow as tf
-import numpy as np
-from matplotlib import pyplot as plt
-import seaborn as sn
-import pandas as pd
 
+import nn.hyperparameters as hp
+from nn.models import Generator, Discriminator
+from util.data.preprocess import Datasets
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-gpu_available = tf.test.is_gpu_available()
+# Killing optional CPU driver warnings
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 
 def parse_args():
-    pass
+	def valid_dir(directory):
+		if os.path.isdir(directory):
+			return os.path.normpath(directory)
+		else:
+			raise ArgumentTypeError(f"Invalid directory: {directory}")
 
+	def valid_model(model_file):
+		if model_file.endswith(".h5"):
+			return os.path.normpath(model_file)
+		else:
+			raise ArgumentTypeError(f"Invalid model file: {model_file}")
 
-def train():
-    pass
+	parser = ArgumentParser(
+		prog="FilterNet",
+		description="A deep learning program for photo-realistic image editing")
 
+	subparsers = parser.add_subparsers()
+	# Subparser for train command
+	tn = subparsers.add_parser(
+		"train",
+		description="Train a new model on the given untouched and edited images")
+	tn.set_defaults(command="train")
 
-def test():
-    pass
+	tn.add_argument("--epochs",
+					type=int, default=hp.num_epochs,
+					help="Number of epochs to train for")
 
+	tn.add_argument("--load-checkpoint",
+					default=None,
+					help="Path to model checkpoint file (should end with the"
+						 "extension .h5)")
 
-def main():
-    pass
+	tn.add_argument("--checkpoint-dir",
+					default=os.getcwd() + "/model_weights",
+					help="Directory to store checkpoint model weights")
+
+	tn.add_argument("--untouched-dir",
+					type=valid_dir,
+					default=os.getcwd() + "/data/train/untouched",
+					help="Directory of untouched images for training")
+
+	tn.add_argument("--edited-dir",
+					type=valid_dir,
+					default=os.getcwd() + "/data/train/edited/C",
+					help="Directory of expert edited images for training")
+
+	# Subparser for test command
+	ts = subparsers.add_parser(
+		"test", description="Evaluate the model on the given test data")
+	ts.set_defaults(command="test")
+
+	ts.add_argument("--checkpoint-file",
+					type=valid_model,
+					help="Model weights to use with testing")
+
+	ts.add_argument("--untouched-dir",
+					type=valid_dir,
+					default=os.getcwd() + "/data/test/untouched",
+					help="Directory of untouched images for testing")
+
+	ts.add_argument("--edited-dir",
+					type=valid_dir,
+					default=os.getcwd() + "/data/test/edited",
+					help="Directory of expert edited images for testing")
+
+	return parser.parse_args()
 
 
 # Make arguments global
 ARGS = parse_args()
 
-if __name__ == '__main__':
-    main()
+
+def train():
+	pass
+
+
+def test():
+	pass
+
+
+def main():
+	gpu_available = tf.test.is_gpu_available()
+
+	pass
+
+
+if __name__ == "__main__":
+	main()
