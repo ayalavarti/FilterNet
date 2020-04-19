717c42cf384efd443df94004358f80473715a985
Ben Silverman <benjaminsilverman7@gmail.com>
2020-04-18 22:03:25 -0500

    worked on discriminator model

--- nn/models.py
+++ nn/models.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from tensorflow.python.keras.layers import LeakyReLU
+
 import nn.hyperparameters as hp
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense
 
@@ -19,10 +21,35 @@ class Generator(tf.keras.Model):
 class Discriminator(tf.keras.Model):
     def __init__(self):
         super(Discriminator, self).__init__()
+        self.optimizer = tf.keras.optimizers.Adam(hp.learning_rate)
+        alpha = 0.2
+        stddev = 0.02
+        self.model = tf.keras.Sequential()
+        self.model.add(Conv2D(16, (3,3), strides=(1, 1), padding='same',
+                              kernel_initializer=tf.keras.initializers.RandomNormal(stddev=stddev)))
+        self.model.add(LeakyReLU(alpha))
+        self.model.add(Conv2D(32, (3, 3), strides=(2, 2), padding='same',
+                              kernel_initializer=tf.keras.initializers.RandomNormal(stddev=stddev)))
+        self.model.add(LeakyReLU(alpha))
+        self.model.add(Conv2D(48, (2, 2), strides=(2, 2), padding='same',
+                              kernel_initializer=tf.keras.initializers.RandomNormal(stddev=stddev)))
+        self.model.add(LeakyReLU(alpha))
+        self.model.add(Conv2D(48, (2, 2), strides=(2, 2), padding='same',
+                              kernel_initializer=tf.keras.initializers.RandomNormal(stddev=stddev)))
+        self.model.add(LeakyReLU(alpha))
+        self.model.add(Conv2D(64, (2, 2), strides=(2, 2), padding='same',
+                              kernel_initializer=tf.keras.initializers.RandomNormal(stddev=stddev)))
+        self.model.add(LeakyReLU(alpha))
+        self.model.add(Conv2D(48, (2, 2), strides=(2, 2), padding='same',
+                              kernel_initializer=tf.keras.initializers.RandomNormal(stddev=stddev)))
+        self.model.add(LeakyReLU(alpha))
+        self.model.add(Flatten())
+        self.model.add(Dense(1))
+        # Do we need an activation here?
 
     @tf.function
     def call(self, inputs):
-        pass
+        return self.model(inputs)
 
     @tf.function
     def loss_function(self):
