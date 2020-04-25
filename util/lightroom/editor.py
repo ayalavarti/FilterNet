baee385182236a562d15a21afc3946effabb6de9
georgiagallant333 <georgia.gallant@gmail.com>
2020-04-25 00:00:04 -0400

    fixed array dimensions

--- util/lightroom/editor.py
+++ util/lightroom/editor.py
@@ -1,21 +1,18 @@
-<<<<<<< Updated upstream
+
 import numpy as np
 import math
 import sys
 from skimage.color import rgb2hsv
 from matplotlib.colors import hsv_to_rgb  # skimage didn't have one for some reason
 import cv2  # pip install opencv-python
-=======
-import cv2
 import numpy as np
 from skimage import io, img_as_ubyte, img_as_float32
-import math
-import sys
 from skimage.color import rgb2hsv, hsv2rgb
+
 
 # from matplotlib.colors import hsv_to_rgb  # skimage didn't have one for some reason
 # pip install opencv-python
->>>>>>> Stashed changes
+from util.datasets import Datasets
 
 
 def relu(vector):
@@ -34,15 +31,7 @@ def sigmoid_inversed(vector):  # basically the logit function
 def clarity(photo, parameter):
     # scaling the dimensions -- ask if this size is okay
     scale = max(photo.shape[:2]) / 512.0
-<<<<<<< Updated upstream
-    # getting our parameter
 
-    # parameters have to do with pixel diameter for filter, aand color space smoothing
-    smoothed = cv2.bilateralFilter((photo * 255).astype(np.uint8),
-                                   int(32 * scale), 50, 10 * scale) / 255.0
-
-    editted = photo + (photo - smoothed) * parameter
-=======
     print(scale)
     # getting our parameter
     # parameters have to do with pixel diameter for filter, aand color space smoothing
@@ -52,7 +41,6 @@ def clarity(photo, parameter):
     new_pic /= 255.0
 
     editted = photo + (photo - new_pic) * parameter
->>>>>>> Stashed changes
     return editted
 
 
@@ -71,11 +59,8 @@ def exposure(photo, parameter):
 
 def temp(photo, parameter):
     ''' requires sigmoid_inversed '''
-<<<<<<< Updated upstream
     to_edit = sigmoid_inversed(photo)
-=======
-    to_edit = photo
->>>>>>> Stashed changes
+
     if parameter > 0:
         to_edit[:, :, 1] += parameter * 1.6
         to_edit[:, :, 2] += parameter * 2
@@ -100,72 +85,64 @@ def tint(photo, parameter):
 def whites(hsv_photo, parameter):
     ''' requires hsv '''
     white = parameter + 1
-    new_values = hsv_photo[2] + (hsv_photo[2] * (np.sqrt(white) - 1) * 0.2)
-    return np.array([hsv_photo[0], hsv_photo[1], new_values])
+    new_values = hsv_photo[:,:,2] + (hsv_photo[:,:,2] * (np.sqrt(white) - 1) * 0.2)
+    toReturn = np.dstack([hsv_photo[:,:,0], hsv_photo[:,:,1], new_values])
+    return toReturn
 
 
 def blacks(hsv_photo, parameter):
     ''' requires hsv '''
     black = parameter + 1
-    new_values = hsv_photo[2] + ((1 - hsv_photo[2]) *
+    new_values = hsv_photo[:,:,2] + ((1 - hsv_photo[:,:,2]) *
                                  (np.sqrt(black) - 1) * 0.2)
-    return np.array([hsv_photo[0], hsv_photo[1], new_values])
+    return np.dstack([hsv_photo[:,:,0], hsv_photo[:,:,1], new_values])
 
 
 def highlights(hsv_photo, parameter):
     ''' requires hsv '''
-    values = hsv_photo[2]
+    values = hsv_photo[:,:,2]
     highlights_mask = sigmoid(5 * (values - 1))
 
     return np.array([
-        hsv_photo[0], hsv_photo[1],
+        hsv_photo[:,:,0], hsv_photo[:,:,1],
         1 - (1 - values) * (1 - highlights_mask * parameter * 5)
     ])
 
 
 def shadows(hsv_photo, parameter):
     ''' requires hsv '''
-    values = hsv_photo[2]
+    values = hsv_photo[:,:,2]
     shadows_mask = 1 - sigmoid(5 * values)
 
     return np.array([
-        hsv_photo[0], hsv_photo[1], values * (1 + shadows_mask * parameter * 5)
+        hsv_photo[:,:,0], hsv_photo[:,:,1], values * (1 + shadows_mask * parameter * 5)
     ])
 
 
 def vibrance(hsv_photo, parameter):
     ''' requires hsv '''
     vibrance = parameter + 1
-    sat = hsv_photo[1]
-<<<<<<< Updated upstream
-    vibrance_flag = -sigmoid((sat - 0.5) * 10) + 1
-    return [
-        hsv_photo[0],
-        sat * vibrance * vibrance_flag + sat * (1 - vibrance_flag),
-        hsv_photo[2]
-    ]
-=======
+    sat = hsv_photo[:,:,1]
     print(sat.shape)
     vibrance_flag = -sigmoid((sat - 0.5) * 10) + 1
     print(vibrance_flag.shape)
 
     return np.array([
-        hsv_photo[0],
+        hsv_photo[:,:,0],
         sat * vibrance * vibrance_flag + sat * (1 - vibrance_flag),
-        hsv_photo[2]
+        hsv_photo[:,:,2]
     ])
->>>>>>> Stashed changes
 
 
 def saturation(hsv_photo, parameter):
     ''' requires hsv '''
     sat = parameter + 1
-    sat_array = hsv_photo[1]
+    sat_array = hsv_photo[:,:,1]
     sat_array = sat_array * sat
     sat_array = relu(sat_array)
     sat_array = 1 - relu(1 - sat_array)
 
-    return np.array([hsv_photo[0], sat_array, hsv_photo[2]])
+    return np.array([hsv_photo[:,:,0], sat_array, hsv_photo[:,:,2]])
 
 
 class PhotoEditor():
@@ -189,11 +166,6 @@ class PhotoEditor():
             else:  # hsv conversions
                 hsv_edit = self.edit_funcs[i](rgb2hsv(photo), parameters[i])
                 # note: assumes that our pictures have had values from [0, 1]
-<<<<<<< Updated upstream
-                photo = hsv_to_rgb(hsv_edit)
-
-        return photo
-=======
                 photo = hsv2rgb(hsv_edit)
 
         return photo
@@ -201,12 +173,22 @@ class PhotoEditor():
 
 if __name__ == "__main__":
     editor = PhotoEditor()
-    test_image = img_as_float32(io.imread("./test.png"))
-    # print(test_image[0, 0, 0])
-    # hsv_edit = vibrance(rgb2hsv(test_image[:, :, :3]), 0.1)
-    # print(rgb2hsv(test_image[:, :, :3]).shape)
-    # note: assumes that our pictures have had values from [0, 1]
-    photo = clarity(test_image, 0.5)
+    UNTOUCHED_TRAIN = './sample_data/train/untouched'
+    EDITED_TRAIN = './sample_data/train/edited'
 
-    io.imsave("./output.png", img_as_ubyte(photo.copy()))
->>>>>>> Stashed changes
+    td = Datasets(UNTOUCHED_TRAIN, EDITED_TRAIN, 'train')
+    for b in td.data:
+        npImage = b[0,0,:,:,:].numpy()
+        hsv = rgb2hsv(npImage)
+
+        #test_image = img_as_float32(io.imread(npImage))
+        # print(test_image[0, 0, 0])
+        # hsv_edit = vibrance(rgb2hsv(test_image[:, :, :3]), 0.1)
+        # print(rgb2hsv(test_image[:, :, :3]).shape)
+        # note: assumes that our pictures have had values from [0, 1]
+        photo = whites(hsv, 0.5)
+        photo = hsv2rgb(photo)
+
+        io.imsave("./output.png", photo.copy())
+        print("here")
+        break
