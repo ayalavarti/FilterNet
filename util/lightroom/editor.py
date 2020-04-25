5aea0fd95ca630d604f7d2a0b903cf999ede67c7
leilankim <leilakim2018@gmail.com>
2020-04-24 22:43:26 -0700

    dictionary, testing blurb at end
    *** need to ask about running tests and util folder ***

--- util/lightroom/editor.py
+++ util/lightroom/editor.py
@@ -8,6 +8,7 @@ from skimage.color import rgb2hsv, hsv2rgb
 
 # from matplotlib.colors import hsv_to_rgb  # skimage didn't have one for some reason
 # pip install opencv-python
+from util.datasets import Datasets
 
 
 def relu(vector):
@@ -79,96 +80,110 @@ def tint(sig_inv_photo, parameter):
 def whites(hsv_photo, parameter):
     ''' requires hsv '''
     white = parameter + 1
-    new_values = hsv_photo[2] + (hsv_photo[2] * (np.sqrt(white) - 1) * 0.2)
-    return np.array([hsv_photo[0], hsv_photo[1], new_values])
+    new_values = hsv_photo[:, :, 2] + (hsv_photo[:, :, 2] *
+                                       (np.sqrt(white) - 1) * 0.2)
+    toReturn = np.dstack([hsv_photo[:, :, 0], hsv_photo[:, :, 1], new_values])
+    return toReturn
 
 
 def blacks(hsv_photo, parameter):
     ''' requires hsv '''
     black = parameter + 1
-    new_values = hsv_photo[2] + ((1 - hsv_photo[2]) *
-                                 (np.sqrt(black) - 1) * 0.2)
-    return np.array([hsv_photo[0], hsv_photo[1], new_values])
+    new_values = hsv_photo[:, :, 2] + ((1 - hsv_photo[:, :, 2]) *
+                                       (np.sqrt(black) - 1) * 0.2)
+    return np.dstack([hsv_photo[:, :, 0], hsv_photo[:, :, 1], new_values])
 
 
 def highlights(hsv_photo, parameter):
     ''' requires hsv '''
-    values = hsv_photo[2]
+    values = hsv_photo[:, :, 2]
     highlights_mask = sigmoid(5 * (values - 1))
 
     return np.array([
-        hsv_photo[0], hsv_photo[1],
+        hsv_photo[:, :, 0], hsv_photo[:, :, 1],
         1 - (1 - values) * (1 - highlights_mask * parameter * 5)
     ])
 
 
 def shadows(hsv_photo, parameter):
     ''' requires hsv '''
-    values = hsv_photo[2]
+    values = hsv_photo[:, :, 2]
     shadows_mask = 1 - sigmoid(5 * values)
 
     return np.array([
-        hsv_photo[0], hsv_photo[1], values * (1 + shadows_mask * parameter * 5)
+        hsv_photo[:, :, 0], hsv_photo[:, :, 1],
+        values * (1 + shadows_mask * parameter * 5)
     ])
 
 
 def vibrance(hsv_photo, parameter):
     ''' requires hsv '''
     vibrance = parameter + 1
-    sat = hsv_photo[1]
+    sat = hsv_photo[:, :, 1]
     vibrance_flag = -sigmoid((sat - 0.5) * 10) + 1
 
     return np.array([
-        hsv_photo[0],
+        hsv_photo[:, :, 0],
         sat * vibrance * vibrance_flag + sat * (1 - vibrance_flag),
-        hsv_photo[2]
+        hsv_photo[:, :, 2]
     ])
 
 
 def saturation(hsv_photo, parameter):
     ''' requires hsv '''
     sat = parameter + 1
-    sat_array = hsv_photo[1]
+    sat_array = hsv_photo[:, :, 1]
     sat_array = sat_array * sat
     sat_array = relu(sat_array)
     sat_array = 1 - relu(1 - sat_array)
 
-    return np.array([hsv_photo[0], sat_array, hsv_photo[2]])
+    return np.array([hsv_photo[:, :, 0], sat_array, hsv_photo[:, :, 2]])
 
 
 class PhotoEditor():
 
     # create constants for the photoeditor parameters by adading to nn > hyperparams
     def __init__(self):
-        self.edit_funcs = [
-            clarity, contrast, exposure, temp, tint, whites, blacks,
-            highlights, shadows, vibrance, saturation
-        ]
+        self.edit_funcs = {
+            1: clarity, 2: contrast, 3: exposure, 4: temp, 5: tint, 6: whites, 7: blacks,
+            8: highlights, 9: shadows, 10: vibrance, 11: saturation
+        }
 
-    def __call__(self, photo, parameters):
+    def __call__(self, photos, parameters):
         # does this list have just one photo?
-        for i in range(parameters.size):
-            if (i > 1) and (i < 5):  # for exposure, temperature, and tint
-                editted = self.edit_funcs[i](sigmoid_inversed(photo),
-                                             parameters[i])
-                photo = sigmoid(editted)
-            elif (i == 0) or (i == 1):  # for clarity and contrast
-                self.edit_funcs[i](photo, parameters[i])
-            else:  # hsv conversions
-                hsv_edit = self.edit_funcs[i](rgb2hsv(photo), parameters[i])
-                # note: assumes that our pictures have had values from [0, 1]
-                photo = hsv2rgb(hsv_edit)
-
-        return photo
+        for photo in photos: 
+            for i in self.edit_funcs.values():
+                if (i == exposure) or (i == temperature) or (i == tint):  # for exposure, temperature, and tint
+                    editted = self.edit_funcs.get(i)(sigmoid_inversed(photo),
+                                                parameters[i])
+                    photo = sigmoid(editted)
+                elif (i == clarity) or (i == contrast):  # for clarity and contrast
+                    self.edit_funcs.get(i)(photo, parameters[i])
+                else:  # hsv conversions
+                    hsv_edit = self.edit_funcsget(i)(rgb2hsv(photo), parameters[i])
+                    # note: assumes that our pictures have had values from [0, 1]
+                    photo = hsv2rgb(hsv_edit)
+        return 
 
 
 if __name__ == "__main__":
     editor = PhotoEditor()
-    test_image = img_as_float32(io.imread("./test.png"))
-    # print(test_image[0, 0, 0])
-    # hsv_edit = vibrance(rgb2hsv(test_image[:, :, :3]), 0.1)
-    # print(rgb2hsv(test_image[:, :, :3]).shape)
-    # note: assumes that our pictures have had values from [0, 1]
-    photo = exposure(test_image, 0.1)
-    photo = sigmoid(photo)
-    io.imsave("./output.png", img_as_ubyte(photo.copy()))
+    UNTOUCHED_TRAIN = './sample_data/train/untouched'
+    EDITED_TRAIN = './sample_data/train/edited'
+
+    td = Datasets(UNTOUCHED_TRAIN, EDITED_TRAIN, 'train')
+    for b in td.data:
+        npImage = b[0, 0, :, :, :].numpy()
+        hsv = rgb2hsv(npImage)
+
+        # test_image = img_as_float32(io.imread("./c-4010.jpg"))
+        # print(test_image[0, 0, 0])
+        # hsv_edit = vibrance(rgb2hsv(test_image[:, :, :3]), 0.1)
+        # print(rgb2hsv(test_image[:, :, :3]).shape)
+        # # note: assumes that our pictures have had values from [0, 1]
+        # photo = blacks(hsv, 0.5)
+        photo = hsv2rgb(photo)
+
+        io.imsave("./output.png", photo.copy())
+        print("here")
+        break
