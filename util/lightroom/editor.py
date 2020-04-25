cf4c93617f2545d37659bcbb4575cd7514cde094
leilankim <leilakim2018@gmail.com>
2020-04-24 18:46:30 -0700

    debugging lightroom
    upload with main function for debugging

--- util/lightroom/editor.py
+++ util/lightroom/editor.py
@@ -1,9 +1,21 @@
+<<<<<<< Updated upstream
 import numpy as np
 import math
 import sys
 from skimage.color import rgb2hsv
 from matplotlib.colors import hsv_to_rgb  # skimage didn't have one for some reason
 import cv2  # pip install opencv-python
+=======
+import cv2
+import numpy as np
+from skimage import io, img_as_ubyte, img_as_float32
+import math
+import sys
+from skimage.color import rgb2hsv, hsv2rgb
+
+# from matplotlib.colors import hsv_to_rgb  # skimage didn't have one for some reason
+# pip install opencv-python
+>>>>>>> Stashed changes
 
 
 def relu(vector):
@@ -22,6 +34,7 @@ def sigmoid_inversed(vector):  # basically the logit function
 def clarity(photo, parameter):
     # scaling the dimensions -- ask if this size is okay
     scale = max(photo.shape[:2]) / 512.0
+<<<<<<< Updated upstream
     # getting our parameter
 
     # parameters have to do with pixel diameter for filter, aand color space smoothing
@@ -29,6 +42,17 @@ def clarity(photo, parameter):
                                    int(32 * scale), 50, 10 * scale) / 255.0
 
     editted = photo + (photo - smoothed) * parameter
+=======
+    print(scale)
+    # getting our parameter
+    # parameters have to do with pixel diameter for filter, aand color space smoothing
+    new_pic = np.array(photo.shape)
+    cv2.bilateralFilter((photo * 255).astype(np.uint8), new_pic,
+                        int(32 * scale), 50, 10 * scale)
+    new_pic /= 255.0
+
+    editted = photo + (photo - new_pic) * parameter
+>>>>>>> Stashed changes
     return editted
 
 
@@ -47,7 +71,11 @@ def exposure(photo, parameter):
 
 def temp(photo, parameter):
     ''' requires sigmoid_inversed '''
+<<<<<<< Updated upstream
     to_edit = sigmoid_inversed(photo)
+=======
+    to_edit = photo
+>>>>>>> Stashed changes
     if parameter > 0:
         to_edit[:, :, 1] += parameter * 1.6
         to_edit[:, :, 2] += parameter * 2
@@ -109,12 +137,24 @@ def vibrance(hsv_photo, parameter):
     ''' requires hsv '''
     vibrance = parameter + 1
     sat = hsv_photo[1]
+<<<<<<< Updated upstream
     vibrance_flag = -sigmoid((sat - 0.5) * 10) + 1
     return [
         hsv_photo[0],
         sat * vibrance * vibrance_flag + sat * (1 - vibrance_flag),
         hsv_photo[2]
     ]
+=======
+    print(sat.shape)
+    vibrance_flag = -sigmoid((sat - 0.5) * 10) + 1
+    print(vibrance_flag.shape)
+
+    return np.array([
+        hsv_photo[0],
+        sat * vibrance * vibrance_flag + sat * (1 - vibrance_flag),
+        hsv_photo[2]
+    ])
+>>>>>>> Stashed changes
 
 
 def saturation(hsv_photo, parameter):
@@ -149,6 +189,24 @@ class PhotoEditor():
             else:  # hsv conversions
                 hsv_edit = self.edit_funcs[i](rgb2hsv(photo), parameters[i])
                 # note: assumes that our pictures have had values from [0, 1]
+<<<<<<< Updated upstream
                 photo = hsv_to_rgb(hsv_edit)
 
         return photo
+=======
+                photo = hsv2rgb(hsv_edit)
+
+        return photo
+
+
+if __name__ == "__main__":
+    editor = PhotoEditor()
+    test_image = img_as_float32(io.imread("./test.png"))
+    # print(test_image[0, 0, 0])
+    # hsv_edit = vibrance(rgb2hsv(test_image[:, :, :3]), 0.1)
+    # print(rgb2hsv(test_image[:, :, :3]).shape)
+    # note: assumes that our pictures have had values from [0, 1]
+    photo = clarity(test_image, 0.5)
+
+    io.imsave("./output.png", img_as_ubyte(photo.copy()))
+>>>>>>> Stashed changes
