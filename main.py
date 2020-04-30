a59a74e5626545d889e7dcf5b41e8dbdbaf5f5eb
leilankim <leilakim2018@gmail.com>
2020-04-29 22:26:57 -0700

    added function for editing a fullsize image

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ import util.visualize as viz
 from nn.models import Generator, Discriminator
 from util.datasets import Datasets
 from util.lightroom.editor import PhotoEditor
+from skimage.metrics import structural_similarity as ssim
+from skimage.transform import resize
 
 # Killing optional CPU driver warnings
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
@@ -21,128 +23,143 @@ print(gpu_available)
 
 
 def parse_args():
-	def valid_dir(directory):
-		if os.path.isdir(directory):
-			return os.path.normpath(directory)
-		else:
-			raise ArgumentTypeError("Invalid directory: {}".format(directory))
+    def valid_dir(directory):
+        if os.path.isdir(directory):
+            return os.path.normpath(directory)
+        else:
+            raise ArgumentTypeError("Invalid directory: {}".format(directory))
 
-	parser = ArgumentParser(
-		prog="FilterNet",
-		description="A deep learning program for photo-realistic image editing")
+    parser = ArgumentParser(
+        prog="FilterNet",
+        description="A deep learning program for photo-realistic image editing"
+    )
 
-	parser.add_argument(
-		"--checkpoint-dir",
-		default=os.getcwd() + "/model_weights",
-		help="Directory to store checkpoint model weights")
+    parser.add_argument("--checkpoint-dir",
+                        default=os.getcwd() + "/model_weights",
+                        help="Directory to store checkpoint model weights")
 
-	parser.add_argument(
-		"--device",
-		type=str,
-		default="GPU:0" if gpu_available else "CPU:0",
-		help="Specify the device of computation eg. CPU:0, GPU:0, GPU:1, ... ")
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="GPU:0" if gpu_available else "CPU:0",
+        help="Specify the device of computation eg. CPU:0, GPU:0, GPU:1, ... ")
 
-	subparsers = parser.add_subparsers()
-	subparsers.required = True
-	subparsers.dest = "command"
+    subparsers = parser.add_subparsers()
+    subparsers.required = True
+    subparsers.dest = "command"
 
-	# Subparser for train command
-	tn = subparsers.add_parser(
-		"train",
-		description="Train a new model on the given untouched and edited images")
-	tn.set_defaults(command="train")
+    # Subparser for train command
+    tn = subparsers.add_parser(
+        "train",
+        description="Train a new model on the given untouched and edited images"
+    )
+    tn.set_defaults(command="train")
 
-	tn.add_argument(
-		"--epochs",
-		type=int, default=hp.num_epochs,
-		help="Number of epochs to train for")
+    tn.add_argument("--epochs",
+                    type=int,
+                    default=hp.num_epochs,
+                    help="Number of epochs to train for")
 
-	tn.add_argument(
-		"--restore-checkpoint",
-		action="store_true",
-		help="Use this flag to resuming training from a previous checkpoint")
+    tn.add_argument(
+        "--restore-checkpoint",
+        action="store_true",
+        help="Use this flag to resuming training from a previous checkpoint")
 
-	tn.add_argument(
-		"--untouched-dir",
-		type=valid_dir,
-		default=os.getcwd() + "/data/train/untouched",
-		help="Directory of untouched images for training")
+    tn.add_argument("--untouched-dir",
+                    type=valid_dir,
+                    default=os.getcwd() + "/data/train/untouched",
+                    help="Directory of untouched images for training")
 
-	tn.add_argument(
-		"--edited-dir",
-		type=valid_dir,
-		default=os.getcwd() + "/data/train/edited",
-		help="Directory of expert edited images for training")
+    tn.add_argument("--edited-dir",
+                    type=valid_dir,
+                    default=os.getcwd() + "/data/train/edited",
+                    help="Directory of expert edited images for training")
 
-	tn.add_argument(
-		"--editor",
-		choices=['A', 'B', 'C', 'D', 'E'],
-		default='C',
-		help="Which editor images to use for training")
+    tn.add_argument("--editor",
+                    choices=['A', 'B', 'C', 'D', 'E'],
+                    default='C',
+                    help="Which editor images to use for training")
 
-	tn.add_argument(
-		"--print-every-x-batches",
-		type=int, default=hp.print_every_x_batches,
-		help="After how many batches you want to print")
+    tn.add_argument("--print-every-x-batches",
+                    type=int,
+                    default=hp.print_every_x_batches,
+                    help="After how many batches you want to print")
 
-	tn.add_argument(
-		"--save-every-x-batches",
-		type=int, default=hp.save_every_x_batches,
-		help="After how many batches you want to save")
+    tn.add_argument("--save-every-x-batches",
+                    type=int,
+                    default=hp.save_every_x_batches,
+                    help="After how many batches you want to save")
 
-	# Subparser for test command
-	ts = subparsers.add_parser(
-		"test", description="Test the model on the given test data")
-	ts.set_defaults(command="test")
+    # Subparser for test command
+    ts = subparsers.add_parser(
+        "test", description="Test the model on the given test data")
+    ts.set_defaults(command="test")
 
-	ts.add_argument(
-		"--untouched-dir",
-		type=valid_dir,
-		default=os.getcwd() + "/data/test/untouched",
-		help="Directory of untouched images for testing")
+    ts.add_argument("--untouched-dir",
+                    type=valid_dir,
+                    default=os.getcwd() + "/data/test/untouched",
+                    help="Directory of untouched images for testing")
 
-	ts.add_argument(
-		"--edited-dir",
-		type=valid_dir,
-		default=os.getcwd() + "/data/test/edited",
-		help="Directory of expert edited images for testing")
+    ts.add_argument("--edited-dir",
+                    type=valid_dir,
+                    default=os.getcwd() + "/data/test/edited",
+                    help="Directory of expert edited images for testing")
 
-	ts.add_argument(
-		"--editor",
-		choices=['C'],
-		default='C',
-		help="Which editor images to use for testing")
+    ts.add_argument("--editor",
+                    choices=['C'],
+                    default='C',
+                    help="Which editor images to use for testing")
 
-	ts.add_argument(
-		"--display",
-		type=bool,
-		default=True,
-		help="True if you want to display the test output, false to save to file.")
+    ts.add_argument(
+        "--display",
+        type=bool,
+        default=True,
+        help=
+        "True if you want to display the test output, false to save to file.")
 
-	ts.add_argument(
-		"--num-display",
-		type=int,
-		default=hp.test_images,
-		help="Number of test images to display, must be <= batch size")
+    ts.add_argument(
+        "--num-display",
+        type=int,
+        default=hp.test_images,
+        help="Number of test images to display, must be <= batch size")
 
-	# Subparser for evaluate command
-	ev = subparsers.add_parser(
-		"evaluate", description="Evaluate / run the model on the given data")
-	ev.set_defaults(command="evaluate")
+    # Subparser for evaluate command
+    ev = subparsers.add_parser(
+        "evaluate", description="Evaluate / run the model on the given data")
+    ev.set_defaults(command="evaluate")
 
-	ev.add_argument(
-		"--image-dir",
-		type=valid_dir,
-		default=os.getcwd() + "/data/test/untouched",
-		help="Directory of untouched images for evaluating")
+    ev.add_argument("--image-dir",
+                    type=valid_dir,
+                    default=os.getcwd() + "/data/test/untouched",
+                    help="Directory of untouched images for evaluating")
 
-	ev.add_argument(
-		"--output-dir",
-		type=valid_dir,
-		default=os.getcwd() + "/output/",
-		help="Directory of output edited images for testing")
+    ev.add_argument("--output-dir",
+                    type=valid_dir,
+                    default=os.getcwd() + "/output/",
+                    help="Directory of output edited images for testing")
 
-	return parser.parse_args()
+    # Subparser for performance command
+    pf = subparsers.add_parser(
+        "performance",
+        description="Evaluate the model by getting PSNR and SSIM metrics")
+    tn.set_defaults(command="performance")
+
+    pf.add_argument("--untouched-dir",
+                    type=valid_dir,
+                    default=os.getcwd() + "/data/train/untouched",
+                    help="Directory of untouched images for training")
+
+    pf.add_argument("--edited-dir",
+                    type=valid_dir,
+                    default=os.getcwd() + "/data/train/edited",
+                    help="Directory of expert edited images for training")
+
+    pf.add_argument("--editor",
+                    choices=['A', 'B', 'C', 'D', 'E'],
+                    default='C',
+                    help="Which editor images to use for training")
+
+    return parser.parse_args()
 
 
 # Make arguments global
@@ -161,14 +178,19 @@ def train(dataset, manager, generator, discriminator):
 					act_scaled, act = generator.convert_prob_act(prob.numpy())
 					act = tf.convert_to_tensor(act)
 
-					y_model = PhotoEditor.edit(x_model.numpy(), act_scaled)
-					y_model = tf.convert_to_tensor(y_model, dtype=tf.float32)
-					d_model = discriminator(x_model)
+                    y_model = editor.PhotoEditor.edit(x_model.numpy(),
+                                                      act_scaled)
+                    y_model = tf.convert_to_tensor(y_model, dtype=tf.float32)
+                    d_model = discriminator(x_model)
 
-					gen_loss = generator.loss_function(x_model, y_model, d_model, prob, act, value)
+                    gen_loss = generator.loss_function(x_model, y_model,
+                                                       d_model, prob, act,
+                                                       value)
 
-				gen_grad = gen_tape.gradient(gen_loss, generator.trainable_variables)
-				generator.optimizer.apply_gradients(zip(gen_grad, generator.trainable_variables))
+                gen_grad = gen_tape.gradient(gen_loss,
+                                             generator.trainable_variables)
+                generator.optimizer.apply_gradients(
+                    zip(gen_grad, generator.trainable_variables))
 
 			# Update Discriminator
 			for i in range(hp.disc_update_freq):
@@ -177,89 +199,145 @@ def train(dataset, manager, generator, discriminator):
 					prob, value = generator(x_model)
 					act_scaled, _ = generator.convert_prob_act(prob.numpy())
 
-					y_model = PhotoEditor.edit(x_model.numpy(), act_scaled)
-					y_model = tf.convert_to_tensor(y_model, dtype=tf.float32)
-					d_expert = discriminator(y_expert)
-					d_model = discriminator(y_model)
+                    y_model = editor.PhotoEditor.edit(x_model.numpy(),
+                                                      act_scaled)
+                    y_model = tf.convert_to_tensor(y_model, dtype=tf.float32)
+                    d_expert = discriminator(y_expert)
+                    d_model = discriminator(y_model)
 
-					disc_loss = discriminator.loss_function(y_model, y_expert, d_model, d_expert)
+                    disc_loss = discriminator.loss_function(
+                        y_model, y_expert, d_model, d_expert)
 
-				disc_grad = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
-				discriminator.optimizer.apply_gradients(zip(disc_grad, discriminator.trainable_variables))
+                disc_grad = disc_tape.gradient(
+                    disc_loss, discriminator.trainable_variables)
+                discriminator.optimizer.apply_gradients(
+                    zip(disc_grad, discriminator.trainable_variables))
 
-			if b % ARGS.print_every_x_batches == 0:
-				print("Epoch: {} Batch: {} Generator Loss: {} Discriminator Loss: {}".format(e, b, gen_loss, disc_loss))
+            if b % ARGS.print_every_x_batches == 0:
+                print(
+                    "Epoch: {} Batch: {} Generator Loss: {} Discriminator Loss: {}"
+                    .format(e, b, gen_loss, disc_loss))
 
 			if b % ARGS.save_every_x_batches == 0:
 				manager.save()
 
 
 def test(dataset, generator):
-	for batch in dataset.data:
-		x_model = batch[:, 0]
-		prob, value = generator(x_model)
-		act_scaled, _ = generator.convert_prob_act(prob.numpy())
+    for batch in dataset.data:
+        x_model = batch[:, 0]
+        prob, value = generator(x_model)
+        act_scaled, _ = generator.convert_prob_act(prob.numpy())
 
-		y_model = PhotoEditor.edit(x_model.numpy(), act_scaled)
-		# Call visualizer to visualize images
-		viz.visualize_batch(batch, y_model, ARGS.display, ARGS.num_display)
+        y_model = editor.PhotoEditor.edit(x_model.numpy(), act_scaled)
+        # Call visualizer to visualize images
+        viz.visualize_batch(batch, y_model, ARGS.display, ARGS.num_display)
 
-		prob, value = generator(x_model)
-		act_scaled, _ = generator.convert_prob_act(prob.numpy(), det=True)
+        prob, value = generator(x_model)
+        act_scaled, _ = generator.convert_prob_act(prob.numpy(), det=True)
 
-		y_model = PhotoEditor.edit(x_model.numpy(), act_scaled)
-		# Call visualizer to visualize images
-		viz.visualize_batch(batch, y_model, ARGS.display, ARGS.num_display)
-		break
+        y_model = editor.PhotoEditor.edit(x_model.numpy(), act_scaled)
+        # Call visualizer to visualize images
+        viz.visualize_batch(batch, y_model, ARGS.display, ARGS.num_display)
+        break
+
+
+def performance(dataset, generator):
+    batch_count = 0
+    psnr = 0
+    psnr_rand = 0
+    ssim_val = 0
+    ssim_rand = 0
+    for batch in dataset.data:
+        x_model = batch[:, 0]
+        prob, value = generator(x_model)
+        act_scaled, _ = generator.convert_prob_act(prob.numpy())
+        y_model = editor.PhotoEditor.edit(x_model.numpy(), act_scaled)
+        rand_act = np.random.rand(batch.shape[0], hp.K)
+        rand_edits = editor.PhotoEditor.edit(x_model.numpy(), rand_act)
+        # Call to get metrics
+        psnr += editor.PSNR(y_model, batch[:, 1])
+        ssim_val += ssim(y_model,
+                         batch[:, 1].numpy(),
+                         data_range=1,
+                         multichannel=True)
+        psnr_rand += editor.PSNR(rand_edits, batch[:, 1])
+        ssim_rand += ssim(rand_edits,
+                          batch[:, 1].numpy(),
+                          data_range=1,
+                          multichannel=True)
+        batch_count += 1
+    print("MODEL -> PSNR: {} SSIM: {} RANDOM -> PSNR: {} SSIM: {}".format(
+        psnr / batch_count, ssim_val / batch_count, psnr_rand / batch_count,
+        ssim_rand / batch_count))
+
+
+def edit_original(big_image, generator):
+    '''
+        Takes in a full-sized image and runs the generator on a smaller version.
+        Returns an edited version of the full-sized image. 
+    '''
+    resized = resize(big_image, (hp.img_size, hp.img_size))
+
+    prob, _ = generator([resized])
+    act_scaled, _ = generator.convert_prob_act(prob.numpy())
+
+    orig_edit = editor.PhotoEditor.edit(big_image.numpy(), act_scaled)
+
+    return orig_edit
 
 
 def main():
-	# Initialize generator and discriminator models
-	generator = Generator()
-	discriminator = Discriminator()
+    # Initialize generator and discriminator models
+    generator = Generator()
+    discriminator = Discriminator()
 
-	# Ensure the checkpoint directory exists
-	sys.enforce_dir(ARGS.checkpoint_dir)
+    # Ensure the checkpoint directory exists
+    sys.enforce_dir(ARGS.checkpoint_dir)
 
-	# Set up tf checkpoint manager
-	checkpoint = tf.train.Checkpoint(
-		generator=generator,
-		discriminator=discriminator)
+    # Set up tf checkpoint manager
+    checkpoint = tf.train.Checkpoint(generator=generator,
+                                     discriminator=discriminator)
 
-	manager = tf.train.CheckpointManager(
-		checkpoint, ARGS.checkpoint_dir,
-		max_to_keep=3)
+    manager = tf.train.CheckpointManager(checkpoint,
+                                         ARGS.checkpoint_dir,
+                                         max_to_keep=3)
 
-	if ARGS.command != 'train' or ARGS.restore_checkpoint:
-		# Restores the latest checkpoint using from the manager
-		print("Loading checkpoint")
-		checkpoint.restore(manager.latest_checkpoint)
+    if ARGS.command != 'train' or ARGS.restore_checkpoint:
+        # Restores the latest checkpoint using from the manager
+        print("Loading checkpoint")
+        checkpoint.restore(manager.latest_checkpoint)
 
-	try:
-		with tf.device("/device:" + ARGS.device):
-			if ARGS.command == "train":
-				# train here!
-				dataset = Datasets(
-					ARGS.untouched_dir, ARGS.edited_dir, "train", ARGS.editor)
+    try:
+        with tf.device("/device:" + ARGS.device):
+            if ARGS.command == "train":
+                # train here!
+                dataset = Datasets(ARGS.untouched_dir, ARGS.edited_dir,
+                                   "train", ARGS.editor)
 
-				train(dataset, manager, generator, discriminator)
+                train(dataset, manager, generator, discriminator)
 
-			if ARGS.command == 'test':
-				# test here!
-				dataset = Datasets(
-					ARGS.untouched_dir, ARGS.edited_dir, "test", ARGS.editor)
-				test(dataset, generator)
+            if ARGS.command == 'test':
+                # test here!
+                dataset = Datasets(ARGS.untouched_dir, ARGS.edited_dir, "test",
+                                   ARGS.editor)
+                test(dataset, generator)
 
-			if ARGS.command == 'evaluate':
-				# Ensure the output directory exists
-				sys.enforce_dir(ARGS.output_dir)
-				# evaluate here!
-				dataset = Datasets(ARGS.image_dir, None, "evaluate")
-				pass
+            if ARGS.command == 'evaluate':
+                # Ensure the output directory exists
+                sys.enforce_dir(ARGS.output_dir)
+                # evaluate here!
+                dataset = Datasets(ARGS.image_dir, None, "evaluate")
+                pass
 
-	except RuntimeError as e:
-		# something went wrong should not get here
-		print(e)
+            if ARGS.command == 'performance':
+                # get performance metrics here!
+                dataset = Datasets(ARGS.untouched_dir, ARGS.edited_dir, "test",
+                                   ARGS.editor)
+                performance(dataset, generator)
+
+    except RuntimeError as e:
+        # something went wrong should not get here
+        print(e)
 
 
 if __name__ == "__main__":
